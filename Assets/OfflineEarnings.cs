64482983088e175f664b70bbe65c2eb5e41b54ce
dpcco <okankrc52@gmail.com>
2019-01-17 19:00:45 +0300

    v1

---
+++ Assets/OfflineEarnings.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class OfflineEarnings : MonoBehaviour
+{
+	DateTime currentDate;
+	DateTime oldDate;
+	int coin = 14;
+	int earned;
+	void Start()
+	{
+		currentDate = System.DateTime.Now;
+		long temp = Convert.ToInt64(PlayerPrefs.GetString("exitTime"));
+		DateTime oldDate = DateTime.FromBinary(temp);
+		Debug.Log("oldDate: " + oldDate);
+		TimeSpan difference = currentDate.Subtract(oldDate);
+		Debug.Log("Difference: " + difference.TotalMinutes);
+		int x = (int)difference.TotalMinutes;
+		earned = coin * x;
+		Debug.Log ("You earned = " + earned);
+		InvokeRepeating("exitDate", 2f, 30f);
+	}
+
+	void exitDate()
+	{
+		PlayerPrefs.SetString("exitTime", System.DateTime.Now.ToBinary().ToString());
+
+		Debug.Log("Last Time " + System.DateTime.Now);
+	}
+
+}
